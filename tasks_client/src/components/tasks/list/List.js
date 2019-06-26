3ab4c72f2f6aa3a6b6f37ea49e375f85a5960d51
Gustakitos <luisgustavolg1033@gmail.com>
2019-06-26 16:41:00 -0300

    crud operations and final styles added

--- tasks_client/src/components/tasks/list/List.js
+++ tasks_client/src/components/tasks/list/List.js
@@ -4,6 +4,30 @@ import Table from 'react-bootstrap/Table'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class List extends Component {
+
+    async checkTask(task) {
+        let form = {'task': {'done': 'true'}}
+        await fetch(`http://localhost:3001/tasks/${task.id}`,
+        {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                task: { done: true }
+            })
+        })
+        this.props.loadTasks()
+    }
+
+    async deleteTask(task) {
+        if (window.confirm(`Are you sure you want to delete: ${task.title} ?`)) {
+            await fetch(`http://localhost:3001/tasks/${task.id}`, {method: 'DELETE'})
+            this.props.loadTasks()
+        }
+    }
+
     render() {
         return (
             <div>
@@ -17,15 +41,15 @@ class List extends Component {
                                     <td className="col-md-10">{task.title}</td>
                                     <td>
                                         {
-                                            task.done == false
+                                            task.done === false
                                             ? <a className="check" href="#">
-                                                <FontAwesomeIcon icon="check-circle"/>
+                                                <FontAwesomeIcon icon="check-circle" onClick={() => this.checkTask(task)} size="lg"/>
                                               </a>
                                             : null
                                         }
                                     </td>
                                     <td>
-                                        <a className="delete" href="#">
+                                        <a className="delete" href="#" onClick={() => this.deleteTask(task)}>
                                             <FontAwesomeIcon icon="trash-alt"/>
                                         </a>
                                     </td>
